5c9888c5ae7ce05729919ba8437ed57357e50e0a
Suvranil Saha <suvranil13@gmail.com>
2023-02-24 17:44:42 +0530

    ok

--- PythonAuto/test/testcase4.py
+++ PythonAuto/test/testcase4.py
@@ -1,10 +1,13 @@
 from behave import *
 from action import *
-from testcase2 import *
 
-I_open_the_browser("https://stage.web.khulke.com/roundtable/all")
-I_log_in_with_email('saikatbhattacharyya631@gmail.com', 'Saikat123@')
 try:
-    I_validate_login_functionality()
-except:
-    print("")
+    open_url("https://stage.web.khulke.com/roundtable/all")
+    click_on_log_in_button()
+    enter_email_for_login('saikatbhattacharyya631@gmail.com')
+    enter_password_for_login('Saikat123@')
+    click_on_login()
+except Exception as e:
+    print(e)
+    
+close_browser()
